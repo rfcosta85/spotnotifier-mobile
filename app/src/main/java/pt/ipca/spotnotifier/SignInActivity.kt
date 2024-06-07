2aeea44b87c9cf2126c7bd1458ae526b0c9544ef
rodrigocosta <rfcosta85@gmail.com>
2024-06-07 23:57:22 +0100

    Configurações do gradle

--- app/src/main/java/pt/ipca/spotnotifier/SignInActivity.kt
+++ app/src/main/java/pt/ipca/spotnotifier/SignInActivity.kt
@@ -1,28 +1,16 @@
 package pt.ipca.spotnotifier
 
-import Spotnotifierapi
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    fun showError(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-    }
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        try {
-            var x = Spotnotifierapi()
-            x.registo_utilizador("a", "b", "c", "a@a.com", "")
-        } catch(e: Exception) {
-            showError("Spotnotifierapi error: " + e.message)
-        }
+        setContentView(R.layout.activity_sign_in)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
