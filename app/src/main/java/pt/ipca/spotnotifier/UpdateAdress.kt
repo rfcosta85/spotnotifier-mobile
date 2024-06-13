82068ad204737a21c61c5136b407121013dfaf35
Francisco <73718340+Franciscopereira2001@users.noreply.github.com>
2024-06-13 20:58:50 +0100

    Atualização ficheiiros

---
+++ app/src/main/java/pt/ipca/spotnotifier/UpdateAdress.kt
@@ -0,0 +1,28 @@
+package pt.ipca.spotnotifier
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UpdateAdress : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_update_adress)
+
+
+        val button = findViewById<ImageButton>(R.id.imageButtonAdresss)
+        button.setOnClickListener {
+            // Finish the current activity and go back to the previous one
+            finish()
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
