b3dc26249f1a7a8c964e9b3645078327f85c93e8
rodrigocosta <rfcosta85@gmail.com>
2024-06-16 19:55:07 +0100

    Implementa a documentação do spotValuation

--- app/src/main/java/pt/ipca/spotnotifier/SpotValuationActivity.kt
+++ app/src/main/java/pt/ipca/spotnotifier/SpotValuationActivity.kt
@@ -25,8 +25,6 @@ class SpotValuationActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         db = FirebaseFirestore.getInstance()
-
-
         radioGroup = findViewById(R.id.radio_group)
         editText = findViewById(R.id.edit_text)
 
@@ -36,6 +34,18 @@ class SpotValuationActivity : AppCompatActivity() {
 
     }
 
+    /**
+     * Handles the click event on the "Send" button.
+     *
+     * This method retrieves the selected radio button and its corresponding text (evaluation)
+     * from the radio group. It then retrieves the message text from the EditText field. If a radio
+     * button
+     * is selected (checked), it calls `sendEvaluation` to send the evaluation and message to the
+     * database.
+     * Otherwise, it displays a toast message indicating that a selection is required.
+     *
+     * @param view The View clicked (usually the "Send" button).
+     */
     private fun onSendButtonClick(view: View) {
         val selectedRadioButtonId = binding.radioGroup.checkedRadioButtonId
         if (selectedRadioButtonId != -1) {
@@ -46,10 +56,27 @@ class SpotValuationActivity : AppCompatActivity() {
             sendEvaluation(evaluation, message)
 
         } else {
-            Toast.makeText(this, "Por favor seleciona uma opção", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Por favor seleciona uma opção",
+                Toast.LENGTH_SHORT).show()
         }
     }
 
+    /**
+     * Sends the user's evaluation and message to the Firestore database.
+     *
+     * This method creates a HashMap named `evaluationData` containing the selected evaluation
+     * and message.
+     * It then adds this data to the "evaluations" collection in Firestore using
+     * `db.collection("evaluations").add`.
+     * It uses `addOnSuccessListener` to handle successful data addition,
+     * displaying a success toast message,
+     * navigating to the MapsActivity, and finishing the current activity.
+     * It uses `addOnFailureListener` to handle
+     * failures, displaying an error toast message.
+     *
+     * @param evaluation The user's selected evaluation (e.g., "Good", "Bad").
+     * @param message The user's message.
+     */
      private fun sendEvaluation(evaluation: String, message: String) {
          val evaluationData = hashMapOf(
              "evaluation" to evaluation,
@@ -59,13 +86,15 @@ class SpotValuationActivity : AppCompatActivity() {
          db.collection("evaluations")
              .add(evaluationData)
              .addOnSuccessListener { documentReference ->
-                 Toast.makeText(baseContext, "Mensagem enviada com sucesso", Toast.LENGTH_SHORT).show()
+                 Toast.makeText(baseContext, "Mensagem enviada com sucesso",
+                     Toast.LENGTH_SHORT).show()
                  val intent = Intent(this, MapsActivity::class.java)
                  startActivity(intent)
                  finish()
              }
              .addOnFailureListener {
-                 Toast.makeText(baseContext, "Erro ao enviar a mensagem", Toast.LENGTH_SHORT).show()
+                 Toast.makeText(baseContext, "Erro ao enviar a mensagem",
+                     Toast.LENGTH_SHORT).show()
              }
     }
 }
